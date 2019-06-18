c6b427f89b36bd434017565c98c030957bca946b
Ivan Enzhaev <8observer8@gmail.com>
2019-06-18 16:19:52 +0400

    Moved the TextFileLoader class to a separate file

---
+++ fl8-1/TextFileLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadingObjFormat
+{
+    class TextFileLoader
+    {
+        // Singleton
+        private TextFileLoader() { }
+        private static TextFileLoader _instance;
+        public static TextFileLoader Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TextFileLoader();
+                }
+                return _instance;
+            }
+        }
+
+        public string LoadTextFile(string fileName)
+        {
+            string fileContent = null;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    fileContent = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TextFileLoader.cs. LoadTextFile(). " + e.Message);
+                return null;
+            }
+            return fileContent;
+        }
+    }
+}
